d04ba5a9638396346e7f412bc415936dc8b075ae
gerry lacuarta <gerry@webprojects101.com>
2014-03-22 13:27:47 -0400

    first commit

---
+++ classes/Listener.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Listener;
+
+/**
+ * Description of Listener
+ *
+ * @author Gerald
+ */
+class Listener {
+    
+    public function __construct() {
+        
+    }
+}
